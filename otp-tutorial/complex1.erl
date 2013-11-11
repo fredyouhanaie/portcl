a7a5bd8f69ff0b9c26a784fce2b9c99269e73203
Fred Youhanaie <fred-gh@anydata.co.uk>
2013-11-11 18:39:59 +0000

    initial commit

---
+++ otp-tutorial/complex1.erl
@@ -0,0 +1,69 @@
+
+%% This is based on the module in the Erlang tutorials.
+%% In fact, almost an exact copy of it!
+
+-module(complex1).
+
+-export([start/1, stop/0, init/1]).
+-export([foo/1, bar/1]).
+-export([start/0]).
+
+start() ->
+	start(["./erlport.tcl"]).
+
+start([ExtPrg]) ->
+	spawn(?MODULE, init, [ExtPrg]).
+
+stop() ->
+	complex ! stop.
+
+foo(X) ->
+	call_port({foo, X}).
+
+bar(Y) ->
+	call_port({bar, Y}).
+
+call_port(Msg) ->
+	complex ! {call, self(), Msg},
+	receive
+		{complex, Result} ->
+			Result
+	end.
+
+init(ExtPrg) ->
+	register(complex, self()),
+	process_flag(trap_exit, true),
+	Port = open_port({spawn, ExtPrg}, [{packet, 2}]),
+	loop(Port).
+
+loop(Port) ->
+	receive
+		{call, Caller, Msg} ->
+			Port ! {self(), {command, encode(Msg)}},
+			receive
+				{Port, {data, Data}} ->
+					Caller ! {complex, decode(Data)}
+			end,
+			loop(Port);
+		stop ->
+			Port ! {self(), close},
+			receive
+				{Port, closed} ->
+					exit(normal)
+			end;
+		{'EXIT', Port, Reason} ->
+			io:format("EXIT: Port=~p, Reason=~p.~n", [Port, Reason]),
+			exit(port_terminated);
+		ANY ->
+			io:format("received/ignored unknown message >~p<~n", [ANY]),
+			loop(Port)
+	end.
+
+encode({foo, X}) ->
+	[1, X band 16#ff];
+
+encode({bar, Y}) ->
+	[2, Y band 16#ff].
+
+decode([Int]) -> Int.
+
---
+++ otp-tutorial/erlport.tcl
@@ -0,0 +1,57 @@
+#!/usr/bin/env tclsh
+
+# erl_comm.tcl
+#	This is the Tcl equivalent of the C ports example found in the
+#	OTP System Documentation tutorial.
+
+set ::header_size 2	;# should match that on the erlang side
+set ::header_fmt S	;# big-endian short
+
+proc diag {msg} {
+	puts stderr "$::argv0: $msg"
+}
+
+proc got_eof {} {
+	diag "got eof"
+	exit
+}
+
+# dump the data bytes as a sequence of decimals
+proc datadump {data} {
+	binary scan $data c* dd
+}
+
+proc foo {X} { return [expr $X + 1] }
+proc bar {Y} { return [expr $Y * 2] }
+
+chan configure stdin  -translation binary
+chan configure stdout -translation binary -buffering none 
+
+while true {
+	# get the header
+	set hdr [read stdin $::header_size]
+	if [chan eof stdin] { got_eof }
+	binary scan $hdr $::header_fmt len
+
+	# get the commad and data
+	set data [read stdin $len]
+	if [chan eof stdin] { got_eof }
+	datadump $data
+	# we expect 2 bytes, command and arg
+	binary scan $data cc cmd arg
+	set cmd [expr $cmd & 0xff]
+	set arg [expr $arg & 0xff]
+
+	# execute the command
+	switch -- $cmd {
+		1	{set res [foo $arg]}
+		2	{set res [bar $arg]}
+		*	{set res -1}
+	}
+
+	# encode and return the result, length and result
+	set data [binary format ${::header_fmt}c 1 [expr $res & 0xff]]
+	datadump $data
+	puts -nonewline stdout $data
+}
+
