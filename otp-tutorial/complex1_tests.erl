7736ee043edd4466f30d018f563ba88c682a883a
Fred Youhanaie <fred-gh@anydata.co.uk>
2014-10-01 02:56:34 +0100

    added eunit tests.

---
+++ otp-tutorial/complex1_tests.erl
@@ -0,0 +1,12 @@
+
+-module(complex1_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+all_test() ->
+	complex1:start(),
+	receive after 500 -> ok end,
+	[?assertEqual( 4, complex1:foo(3)),
+	 ?assertEqual(10, complex1:bar(5))
+	].
+	 
