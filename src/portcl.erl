d8b1d4e1effb9d5c472acb46fd7912b86fa2efd5
Fred Youhanaie <fyrlang@anydata.co.uk>
2023-08-16 14:16:39 +0100

    convert the main code to use rebar3

---
+++ src/portcl.erl
@@ -0,0 +1,11 @@
+%%%-------------------------------------------------------------------
+%%% @author Fred Youhanaie <fyrlang@anydata.co.uk>
+%%% @copyright 2021, Fred Youhanaie
+%%% @doc
+%%%
+%%% @end
+%%% Created :  2023-08-12 by Fred Youhanaie <fyrlang@anydata.co.uk>
+%%%-------------------------------------------------------------------
+-module(portcl).
+
+-export([]).
