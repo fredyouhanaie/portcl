664c503c6ff04f59f59d27e96d5fe4c5d43e4442
Fred Youhanaie <fyrlang@anydata.co.uk>
2023-08-17 18:19:57 +0100

    fix the problem with child termination messages
    It seems the child exit notification was changed during the last 9
    years! We now receive an "exit_status" message, instead of `EXIT`.
    
    We are now using logger for printing messages.

--- Examples/buttons/src/buttons.erl
+++ Examples/buttons/src/buttons.erl
@@ -14,7 +14,11 @@
 
 %% API exports
 -export([main/1]).
--export([start/0]).
+-export([start/0, start/1]).
+
+%%--------------------------------------------------------------------
+
+-include_lib("kernel/include/logger.hrl").
 
 %%--------------------------------------------------------------------
 
@@ -27,16 +31,25 @@
 %%====================================================================
 
 start() ->
-    erlang:open_port(?TclCmd, [ ?PortMode, {args, ?TclArgs} ]),
-    erlang:open_port(?TclCmd, [ ?PortMode, {args, ?TclArgs} ]),
+    start([]).
+
+%%--------------------------------------------------------------------
+
+start(Args) ->
+    logger:set_primary_config(#{level => notice}),
+    logger:set_handler_config(default, formatter, {logger_formatter, #{}}),
+
+    ?LOG_NOTICE("Args=~p.", [Args]),
+
+    erlang:open_port(?TclCmd, [ ?PortMode, {args, ?TclArgs}, exit_status ]),
+    erlang:open_port(?TclCmd, [ ?PortMode, {args, ?TclArgs}, exit_status ]),
     loop(2).
 
 %%--------------------------------------------------------------------
 %% escript Entry point
 %%
 main(Args) ->
-    io:format("Args: ~p~n", [Args]),
-    start(),
+    start(Args),
     erlang:halt(0).
 
 %%====================================================================
@@ -44,22 +57,22 @@ main(Args) ->
 %%====================================================================
 
 loop(0) ->
-    io:format("no more ports, signing off!~n"),
+    ?LOG_NOTICE("no more ports, signing off!"),
     ok;
 
 loop(N) ->
     receive
         {Port, {data, Msg}} when is_port(Port) ->
-            io:format("Port ~p: received ~p.~n", [Port, Msg]),
+            ?LOG_NOTICE("Port ~p: received ~p.", [Port, Msg]),
             loop(N);
-        {'EXIT', Port, normal} when is_port(Port) ->
-            io:format("Port ~p: received EXIT.~n", [Port]),
+        {Port, {exit_status, Status}} when is_port(Port) ->
+            ?LOG_NOTICE("Port ~p: terminated, status=~p.", [Port, Status]),
             loop(N-1);
         ANY ->
-            io:format("unexpected message: ~p.~n", [ANY]),
+            ?LOG_NOTICE("unexpected message: ~p.", [ANY]),
             loop(N)
     after 5000 ->
-            io:format("TICK~n"),
+            ?LOG_NOTICE("TICK"),
             loop(N)
     end.
 
